8d074cb8ea1db46779c89a6ea76e726e65e44f95
ouafcode <ouafaesaim@gmail.com>
2024-08-06 12:41:09 +0000

    task1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,3 +12,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(regex, f"{field}={redaction}",
                          message)
     return message
+
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        log_format = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, log_format, self.SEPARATOR)
