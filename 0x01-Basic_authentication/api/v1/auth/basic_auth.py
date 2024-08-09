fbb74fc4b1036db34007189a07f0a6c003543f94
ouafcode <ouafaesaim@gmail.com>
2024-08-09 15:52:22 +0000

    task12

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,13 +37,14 @@ class BasicAuth(Auth):
     def extract_user_credentials(
             self, decoded_base64_authorization_header: str) -> (str, str):
         """ User Cridentials """
-        if decoded_base64_authorization_header is None:
-            return (None, None)
-        if type(decoded_base64_authorization_header) is not str:
-            return (None, None)
-        if ':' not in decoded_base64_authorization_header:
-            return (None, None)
-        return tuple(decoded_base64_authorization_header.split(':'))
+        if isinstance(decoded_base64_authorization_header, str):
+            index = decoded_base64_authorization_header.find(":")
+            if index != -1:
+                return (
+                    decoded_base64_authorization_header[:index],
+                    decoded_base64_authorization_header[index + 1:],
+                )
+        return (None, None)
 
     def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
