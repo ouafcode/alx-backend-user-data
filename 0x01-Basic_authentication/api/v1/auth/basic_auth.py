bfa6c9ca10d934ba61f7753bbaf5627af2352515
ouafcode <ouafaesaim@gmail.com>
2024-08-09 15:17:01 +0000

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 from typing import List, TypeVar
 from flask import request
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,16 @@ class BasicAuth(Auth):
         if authorization_header[:6] != "Basic ":
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Base64 decode """
+        if base64_authorization_header is None:
+            return None
+        if isinstance(base64_authorization_header, str):
+            try:
+                decode_byte = base64.b64decode(base64_authorization_header)
+                decode_str = decode_byte.decode("utf-8")
+                return decode_str
+            except (base64.binascii.Error, UnicodeDecodeError) as error:
+                return None
