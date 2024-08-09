ab2a0f54687acbdaff18e080325876a5ffc4d528
ouafcode <ouafaesaim@gmail.com>
2024-08-09 14:49:33 +0000

    task7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,7 +7,9 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Basic Authentification """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+                 self, authorization_header: str
+    ) -> str:
         """ extract base64 """
         if authorization_header is None:
             return None
