ee53d0660f73a6a889c683652ccf1623a98c44ac
ouafcode <ouafaesaim@gmail.com>
2024-08-15 12:29:21 +0000

    task1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,7 +3,7 @@
 from typing import List, TypeVar
 from flask import request
 from api.v1.auth.auth import Auth
-from model.user import User
+from models.user import User
 
 
 class SessionAuth(Auth):
