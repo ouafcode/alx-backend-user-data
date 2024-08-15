0933d3c41179842e8d5509b22b8136e5263fee71
ouafcode <ouafaesaim@gmail.com>
2024-08-15 12:44:53 +0000

    task0

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """doc doc doc """
 from typing import List, TypeVar
+from uuid import uuid4
 from flask import request
 from api.v1.auth.auth import Auth
 from models.user import User
@@ -8,4 +9,14 @@ from models.user import User
 
 class SessionAuth(Auth):
     """ docs docs """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ docs docs """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        Session_ID = uuid4()
+        self.user_id_by_session_id[str(Session_ID)] = user_id
+        return str(Session_ID)        
